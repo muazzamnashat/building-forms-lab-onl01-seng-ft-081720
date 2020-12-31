aa8ed3389d620ce2fb044997e66f40ef97ef49a8
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-30 23:39:34 -0500

    commit tree 137b2d8b04b8149dc9fc31dfeaedba71d6f2f7cf

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,9 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  if (action.type === 'ADD_BAND') {
+    // state.bands.push(action.band);
+    return {bands: state.bands.concat(action.band)}
+  }
+  else return state
 };
